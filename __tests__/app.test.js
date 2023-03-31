020706279287a550d39f56a5b8337531397470e9
ah28011992 <hedgecockaaron@gmail.com>
2023-03-31 10:23:07 +0100

    update test file

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,7 +1,7 @@
 const app = require('../app')
 const request = require('supertest')
 const db = require('../db/connection')
-
+const seed = require('../db/seeds/seed')
 const testData = require('../db/data/test-data/index')
 const endPoints = require('../endpoints.json')
 
@@ -12,3 +12,9 @@ beforeEach(() => {
 afterAll(() => {
     return db.end();
 });
+
+describe('app', () => {
+    it('should ', () => {
+        
+    });
+})
